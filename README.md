29c54410dbe5782097c4d672a6f2ca36beac6d57
Brady Aiello <brady.aiello@gmail.com>
2020-07-22 08:48:53 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenAPI
+Working through Mitch Tabian's Powerful Android Apps with Jetpack course.
+
+https://codingwithmitch.com/courses/powerful-android-apps-with-jetpack-architecture/
+https://github.com/mitchtabian/Open-API-Android-App
---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("29.0.3")
+
+    defaultConfig {
+        applicationId = "com.codingwithmitch.openapi"
+        minSdkVersion(23)
+        targetSdkVersion(30)
+        versionCode(1)
+        versionName("1.0")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val kotlinVersion = rootProject.extra.get("kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    // Unit Test
+    testImplementation("junit:junit:4.13")
+
+    // Integration Test
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+
+    val retrofit2Version = "2.6.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofit2Version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit2Version")
+
+    val lifecycleVersion = "2.3.0-alpha05"
+    implementation("androidx.lifecycle:lifecycle-runtime:$lifecycleVersion")
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
+    val coroutinesVersion = "1.3.6"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    val roomVersion = "2.2.5"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    val daggerVersion = "2.28.3"
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+
+    val navigationVersion = "2.3.0"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-runtime:$navigationVersion")
+
+    implementation("com.google.android.material:material:1.3.0-alpha01")
+    implementation("com.afollestad.material-dialogs:core:3.3.0")
+    implementation("com.squareup.leakcanary:leakcanary-android:2.4")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.0-alpha04")
+
+    val glideVersion = "4.11.0"
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    annotationProcessor("com.github.bumptech.glide:compiler:$glideVersion")
+}
