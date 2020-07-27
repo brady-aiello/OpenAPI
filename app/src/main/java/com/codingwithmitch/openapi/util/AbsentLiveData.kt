2ce2b7069ed6e1bc34d934609fec96fb4eb6c284
Brady Aiello <brady.aiello@gmail.com>
2020-07-27 15:39:31 -0700

    Part 1 Lesson 6 MVI Architecture Generics (DataState, Events, Wrappers) https://codingwithmitch.com/courses/powerful-android-apps-with-jetpack-architecture/mvi-architecture-generics-datastate-events-wrapper/ Adding some generic scaffolding that will be reused throughout the app.

---
+++ app/src/main/java/com/codingwithmitch/openapi/util/AbsentLiveData.kt
@@ -0,0 +1,12 @@
+package com.codingwithmitch.openapi.util
+
+import androidx.lifecycle.LiveData
+
+class AbsentLiveData <T: Any?> private constructor(): LiveData<T>() {
+    init {
+        postValue(null)
+    }
+    companion object {
+        fun <T> create(): LiveData<T> = AbsentLiveData()
+    }
+}
